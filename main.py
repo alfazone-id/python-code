112ea3465e63e1f1b0b5db2afec37e002962f2a0
AYMANEDLYN <blackman2774@gmail.com>
2023-05-31 10:23:03 +0700

    sekuensial percabangan perulangan

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 """
 project pertama dengan phyton
+semua sintaksis dasar bahasa pemograman terdiri dari :
+1. sekuensial : langkah berurutan
+2. percabangan : langkah melompat jika kondisi terpenuhi
+3. perulangan : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print("hello world")
-print("My name luki")
+print('ibu berkata')
+print('budi menjawab, "baik, apa yang harus saya lakukan di toko"')
+print('ibu menjawab, "beli satu botol susu, dan jika ada telor beli 6"')
+print('maka budi berangkat ke toko')
+print('dan mulai berbelanja')
